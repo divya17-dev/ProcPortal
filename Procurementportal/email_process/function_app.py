1e1b0bdec7d91d340d1935410e3e81b23577b7b9
pavanitr <pavan.bezawada@itradiant.com>
2025-06-10 02:21:25 +0530

    Update function_app.py

--- Procurementportal/email_process/function_app.py
+++ Procurementportal/email_process/function_app.py
@@ -6,7 +6,15 @@ import traceback
 
 # Ensure main.py is importable
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from email_process.extraction import start_email_monitoring  # assuming function is in main.py
+#from email_process.extraction import start_email_monitoring  # assuming function is in main.py
+try:
+    from email_process.extraction import fetch_sap_data
+    print("✅ Imported successfully")
+except Exception as e:
+    import traceback
+    print("❌ Import failed:", e)
+    traceback.print_exc()
+
 
 
 def main(mytimer: func.TimerRequest) -> None:
