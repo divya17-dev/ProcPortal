345263e001b570744b5a2fa2f0f374f1c2f4b49b
divya17-dev <divya.mehra@itradiant.com>
2025-06-10 10:44:16 +0530

    Update function_app.py

--- Procurementportal/email_process/function_app.py
+++ Procurementportal/email_process/function_app.py
@@ -6,14 +6,8 @@ import traceback
 
 # Ensure main.py is importable
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-#from email_process.extraction import start_email_monitoring  # assuming function is in main.py
-try:
-    from email_process.extraction import fetch_sap_data
-    print("✅ Imported successfully")
-except Exception as e:
-    import traceback
-    print("❌ Import failed:", e)
-    traceback.print_exc()
+from email_process.extraction import start_email_monitoring  
+
 
 
 
@@ -21,7 +15,7 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.info("Timer trigger executed.")
     
     try:
-        start_email_monitoring()
+        start_email_monitoring
     except Exception as e:
         logging.error(f"Error during email monitoring: {e}")
         logging.error("Traceback:\n" + traceback.format_exc())
