244151030763b1f7aa4496d23b57888ccf681aa0
divya17-dev <divya.mehra@itradiant.com>
2025-06-10 10:55:26 +0530

    Update function_app.py

--- Procurementportal/email_process/function_app.py
+++ Procurementportal/email_process/function_app.py
@@ -1,21 +1,18 @@
 import logging
 import azure.functions as func
-import sys
-import os
 import traceback
 
-# Ensure main.py is importable
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from email_process.extraction import start_email_monitoring  
-
-
-
+# Fix import 
+try:
+    from .extraction import start_email_monitoring
+except ImportError:
+    from email_process.extraction import start_email_monitoring  # fallback
 
 def main(mytimer: func.TimerRequest) -> None:
     logging.info("Timer trigger executed.")
     
     try:
-        start_email_monitoring
+        start_email_monitoring()  
     except Exception as e:
         logging.error(f"Error during email monitoring: {e}")
         logging.error("Traceback:\n" + traceback.format_exc())
